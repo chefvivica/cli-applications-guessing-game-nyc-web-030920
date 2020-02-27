51d5ca610adcca620c02b088637a91733c1c45d9
Vivica Ma <vivicacongma@gmail.com>
2020-02-27 19:15:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,14 @@
 
 def run_guessing_game(input)
   anwser = rand(1..6)
-    unless input == anwser
-    puts "please enter the number between 1 - 6"
-    gets.chomp
-  end
+
+  puts "please enter the number between 1 - 6"
+  gets.chomp
+
+ if input == anwser 
+  puts "You guessed the correct number!"
+ 
+ elsif
+  puts "Sorry! The computer guessed #{anwser}"
+  
+  
